ae4568a1583da0afd8342c7a5e10d876e72ef432
firemax13 <mydadisbrave2@gmail.com>
2020-09-26 01:21:23 +0000

    Revert "scripts: Makefile.lib: Dont export EXT"
    This reverts commit 7685af033832669e34d136f9cf5470c91159d078.

--- fire.sh
+++ fire.sh
@@ -11,9 +11,9 @@ TOOLCHAIN_PATH=/home/Vevelo/firemax13/aarch64-linux-android
 export CROSS_COMPILE=$TOOLCHAIN_PATH/bin/aarch64-linux-android-
 export ARCH=arm64
 
-# KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc"
+KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc"
 
-make -C $(pwd) O=$(pwd)/out ARCH=arm64 KCFLAGS=-mno-android a20s_eur_open_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out ARCH=arm64 KCFLAGS=-mno-android
+make -C $(pwd) O=$(pwd)/out ARCH=arm64 $KERNEL_MAKE_ENV KCFLAGS=-mno-android a20s_eur_open_defconfig
+make -j8 -C $(pwd) O=$(pwd)/out ARCH=arm64 $KERNEL_MAKE_ENV KCFLAGS=-mno-android
 
 tools/mkdtimg create out/arch/arm64/boot/dtbo.img --page_size=2048 $(find out -name "*.dtbo")
